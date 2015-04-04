57ab335f85ab03a65075b61bc90ae53ebf8b3f6c
Dmitri Soshnikov <dmitri@soshnikov.com>
2015-04-04 03:26:38 +0300

    Version 0 - No formatting changes

---
+++ TwitterUniversal/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TwitterUniversal.Model;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TwitterUniversal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+		TwitterSearchResult Res;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+			Res = new TwitterSearchResult();
+			DataContext = Res;
+        }
+
+		private void SearchTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+		{
+			if (e.Key == Windows.System.VirtualKey.Enter)
+			{
+				LoadData();
+			}
+		}
+
+		private async void LoadData()
+		{
+			await Res.Search(SearchTextBox.Text.Trim());
+		}
+
+	}
+}
